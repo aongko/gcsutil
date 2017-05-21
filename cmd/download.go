79da4c6d681eef38e24ec0c65d3b05e5c8b08f01
Andrew Ongko <Andrew.Ongko@gmail.com>
2017-05-21 23:15:28 +0700

    Supports listing objects in one or more bucket(s) and download object(s) from a bucket

---
+++ cmd/download.go
@@ -0,0 +1,115 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/storage"
+
+	"github.com/spf13/cobra"
+)
+
+var bucket string
+
+// downloadCmd represents the download command
+var downloadCmd = &cobra.Command{
+	Use:   "download bucket object",
+	Short: "Download file(s) in the bucket",
+	Long: `Download file(s) from the specified bucket into current directories.
+For filename(s) with "/", directory(-ies) will be created as well.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("need bucket and object name")
+		}
+		ctx := context.Background()
+		client, err := storage.NewClient(ctx)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		fmt.Println("Current working directory: " + wd)
+
+		bucket = args[0]
+		for _, object := range args[1:] {
+			fmt.Printf("Downloading %v:%v\n", bucket, object)
+
+			data, err := read(client, bucket, object)
+			if err != nil {
+				log.Fatalf("Cannot read object: %v", err)
+				return err
+			}
+
+			d := strings.Split(object, "/")
+			var fullPath string
+			if len(d) > 1 {
+				dirs := strings.Join(d[:len(d)-1], "/")
+				dir := wd + "/" + dirs
+				filename := d[len(d)-1]
+				fullPath = dir + "/" + filename
+				err = os.MkdirAll(dir, 0755)
+				if err != nil {
+					return err
+				}
+			} else {
+				filename := object
+				fullPath = wd + "/" + filename
+			}
+
+			ioutil.WriteFile(fullPath, data, 0644)
+		}
+		return nil
+	},
+}
+
+func read(client *storage.Client, bucket, object string) ([]byte, error) {
+	ctx := context.Background()
+	// [START download_file]
+	rc, err := client.Bucket(bucket).Object(object).NewReader(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rc.Close()
+
+	data, err := ioutil.ReadAll(rc)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+	// [END download_file]
+}
+
+func init() {
+	RootCmd.AddCommand(downloadCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// downloadCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// downloadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/list.go
@@ -0,0 +1,126 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
+
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list bucket [bucket...]",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("no bucket specified")
+		}
+		ctx := context.Background()
+		client, err := storage.NewClient(ctx)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		for _, bucket := range args {
+			list(os.Stdout, client, bucket)
+		}
+		return nil
+	},
+}
+
+func list(w io.Writer, client *storage.Client, bucket string) error {
+	ctx := context.Background()
+	// [START storage_list_files]
+	it := client.Bucket(bucket).Objects(ctx, nil)
+	fmt.Fprintln(w, "Bucket: "+bucket)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, attrs.Name)
+	}
+	// [END storage_list_files]
+	return nil
+}
+
+func listByPrefix(w io.Writer, client *storage.Client, bucket, prefix, delim string) error {
+	ctx := context.Background()
+	// [START storage_list_files_with_prefix]
+	// Prefixes and delimiters can be used to emulate directory listings.
+	// Prefixes can be used filter objects starting with prefix.
+	// The delimiter argument can be used to restrict the results to only the
+	// objects in the given "directory". Without the delimeter, the entire  tree
+	// under the prefix is returned.
+	//
+	// For example, given these blobs:
+	//   /a/1.txt
+	//   /a/b/2.txt
+	//
+	// If you just specify prefix="a/", you'll get back:
+	//   /a/1.txt
+	//   /a/b/2.txt
+	//
+	// However, if you specify prefix="a/" and delim="/", you'll get back:
+	//   /a/1.txt
+	it := client.Bucket(bucket).Objects(ctx, &storage.Query{
+		Prefix:    prefix,
+		Delimiter: delim,
+	})
+	fmt.Fprintln(w, "Bucket: "+bucket)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, attrs.Name)
+	}
+	// [END storage_list_files_with_prefix]
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
